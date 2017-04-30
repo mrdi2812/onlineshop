e85ff2f288815fd6254bcffcdb31811658d6a059
TUAN-PC\TUAN <anhtuan281290@gmail.com>
2017-04-30 13:15:44 +0700

    test code

---
+++ OnlineShop.UnitTest/RepositoryTest/PostCategoryRepositoryTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnlineShop.Data.Infrastructure;
+using OnlineShop.Data.Repositories;
+using OnlineShop.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.UnitTest.RepositoryTest
+{
+    [TestClass]
+    public class PostCategoryRepositoryTest
+    {
+        IDbFactory dbFactory;
+        IPostCategoryRepository objCategoryRepository;
+        IUnitOfWork unitOfWork;
+        [TestInitialize]
+        public void Initialize()
+        {
+            dbFactory = new DbFactory();
+            objCategoryRepository = new PostCategoryRepository(dbFactory);
+            unitOfWork = new UnitOfWork(dbFactory);
+        }
+        [TestMethod]
+        public void PostCategory_Repository_Create()
+        {
+            PostCategory category = new PostCategory();
+            category.Name = "Test category";
+            category.Alias = "Test-category";
+            category.Status = true;
+
+            var result = objCategoryRepository.Add(category);
+            unitOfWork.Commit();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.ID);
+        }
+        [TestMethod]
+        public void PostCategory_Repository_GetAll()
+        {
+            var list = objCategoryRepository.GetAll().ToList();
+            Assert.AreEqual(1, list.Count);
+
+        }
+    
+    }
+}
---
+++ OnlineShop.UnitTest/ServiceTest/PostCategoryServiceTest.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using OnlineShop.Data.Infrastructure;
+using OnlineShop.Data.Repositories;
+using OnlineShop.Model.Models;
+using OnlineShop.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.UnitTest.ServiceTest
+{
+    [TestClass]
+    public class PostCategoryServiceTest
+    {
+        private Mock<IPostCategoryRepository> _moqRepository;
+        private Mock<IUnitOfWork> _moqUnitOfWork;
+        private IPostCategoryService _categoryService;
+        List<PostCategory> listCategory;
+        [TestInitialize]
+        public void Initialize()
+        {
+            _moqRepository = new Mock<IPostCategoryRepository>();
+            _moqUnitOfWork = new Mock<IUnitOfWork>();
+            _categoryService = new PostCategoryService(_moqRepository.Object, _moqUnitOfWork.Object);
+            listCategory = new List<PostCategory>
+            {
+                new PostCategory() {ID=1,Name="1",Status=true },
+                new PostCategory() {ID=2,Name="2",Status=true },
+                new PostCategory() {ID=3,Name="3",Status=true }
+            };
+        }
+        [TestMethod]
+        public void PostCategory_Service_GetAll()
+        {
+            _moqRepository.Setup(m => m.GetAll(null)).Returns(listCategory);
+            var result = _categoryService.GetAll() as List<PostCategory>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Count);
+        }
+        [TestMethod]
+        public void PostCategory_Service_Create()
+        {
+            PostCategory postCategory = new PostCategory();
+            postCategory.Name = "Test 1";
+            postCategory.Alias = "Test Alias";
+            postCategory.Status = true;
+            _moqRepository.Setup(m => m.Add(postCategory)).Returns((PostCategory p) =>
+            {
+                p.ID = 1;
+                return p;
+            });
+            var result = _categoryService.Add(postCategory);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.ID);
+        }
+    }
+}
