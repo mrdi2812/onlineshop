9212eae38a3474ef8359b9690609d918e2a1d399
TUAN-PC\TUAN <anhtuan281290@gmail.com>
2017-05-16 23:50:13 +0700

    trình thông báo từ service đến người dùng

--- OnlineShop.Service/ProductCategoryService.cs
+++ OnlineShop.Service/ProductCategoryService.cs
@@ -15,7 +15,7 @@ namespace OnlineShop.Service
         ProductCategory Delete(int id);
 
         IEnumerable<ProductCategory> GetAll();
-
+        IEnumerable<ProductCategory> GetAll(string keyword);
         IEnumerable<ProductCategory> GetAllByParentId(int parentId);
 
         ProductCategory GetById(int id);
@@ -46,6 +46,14 @@ namespace OnlineShop.Service
         public IEnumerable<ProductCategory> GetAll()
         {
             return _productCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<ProductCategory> GetAll(string keyword)
+        {
+            if (!string.IsNullOrEmpty(keyword))
+                return _productCategoryRepository.GetMulti(m => m.Name.Contains(keyword) || m.Description.Contains(keyword));
+            else
+                return _productCategoryRepository.GetAll();
         }
 
         public IEnumerable<ProductCategory> GetAllByParentId(int parentId)
--- OnlineShop.WebAPI/app/components/product_categories/productCategoryListController.js
+++ OnlineShop.WebAPI/app/components/product_categories/productCategoryListController.js
@@ -1,23 +1,34 @@
 ﻿(function (app) {
     app.controller('productCategoryListController', productCategoryListController);
 
-    productCategoryListController.$inject = ['$scope', 'apiService'];
+    productCategoryListController.$inject = ['$scope', 'apiService','notificationService'];
 
-    function productCategoryListController($scope, apiService) {
+    function productCategoryListController($scope, apiService, notificationService) {
         $scope.productCategories = [];
         $scope.page = 0;
         $scope.pagesCount = 0;
         $scope.getProductCagories = getProductCagories;
-
+        $scope.keyword = '';
+        $scope.search = search;
+        function search() {
+            getProductCagories();
+        }
         function getProductCagories(page) {
             page = page || 0;
             var config = {
                 params: {
+                    keyword:$scope.keyword,
                     page: page,
                     pageSize: 20
                 }
             }
             apiService.get('/api/productcategory/getall', config, function (result) {
+                if (result.data.TotalCount == 0) {
+                    notificationService.displayWarning('Không có bản ghi nào được tìm thấy.');
+                }
+                else {
+                    notificationService.displaySuccess('Đã tìm thấy ' + result.data.TotalCount + ' bản ghi.');
+                }
                 $scope.productCategories = result.data.Items;
                 $scope.page = result.data.Page;
                 $scope.pagesCount = result.data.TotalPages;
