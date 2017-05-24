9d3dd6ba38028ab002e7f912ad3dfb75e9b20446
TUAN-PC\TUAN <anhtuan281290@gmail.com>
2017-05-24 23:09:24 +0700

    login using abstract base index

--- OnlineShop.WebAPI/app/app.js
+++ OnlineShop.WebAPI/app/app.js
@@ -9,8 +9,20 @@
     config.$inject = ['$stateProvider', '$urlRouterProvider'];
 
     function config($stateProvider, $urlRouterProvider) {
-        $stateProvider.state('home', {
-            url: "/admin",
+        $stateProvider
+            .state('base',{
+                url: "",
+                templateUrl: "/app/shared/views/baseView.html",
+                abstract:true
+            })
+            .state('login', {
+                url: "/login",
+                templateUrl: "/app/components/login/loginView.html",
+                controller: "loginController"
+            })
+            .state('home', {
+                url: "/admin",
+                parent:'base',
             templateUrl: "/app/components/home/homeView.html",
             controller: "homeController"
         });
---
+++ OnlineShop.WebAPI/app/components/login/loginController.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="/Assets/admin/libs/angular/angular.js" />
+
+(function (app) {
+    app.controller('loginController', loginController);
+    loginController.$inject = ['$scope','$state','apiService'];
+    function loginController($scope, $state, apiService) {
+        $scope.loginSubmit = function(){
+            $state.go('home');
+        }
+    }
+})(angular.module('onlineshop'));
